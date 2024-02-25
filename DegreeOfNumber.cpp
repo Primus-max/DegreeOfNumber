e18f13b337233128eb37d4643c40f0058d1545e4
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-25 06:17:51 +0300

    Добавьте файлы проекта.

---
+++ DegreeOfNumber.cpp
@@ -0,0 +1,32 @@
+﻿// Написать функцию, которая принимает два
+// параметра: основание степени и показатель степени, и
+// вычисляет степень числа на основе полученных данных.
+
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+template<typename T>
+T CalcDegree(T A, T B);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int num1 = 0, num2 = 0;
+    cout << "Чтобы узнать степень числа введите число основание и показатель : ";
+    cin >> num1 >> num2;
+
+    cout << "Степень числа : " << num1 << " = " << CalcDegree(num1, num2)<< endl;
+}
+
+template<typename T>
+T CalcDegree(T A, T B) {
+    T result = 1;
+    for (size_t i = 0; i < B; i++)
+        result *= A;
+
+    return result;
+}
